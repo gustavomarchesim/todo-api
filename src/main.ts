7169c1b4e2ec02e98f88d8e89bf1eaaec2a5cc7a
Gustavo Marchesim <105670464+gustavomarchesim@users.noreply.github.com>
2023-07-14 18:24:08 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['debug', 'error', 'warn', 'log'],
+  });
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: false,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(3000);
+}
+
+bootstrap();
